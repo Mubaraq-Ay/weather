7d87ea4afc5e7a212f161f410caa045870e38363
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-09-10 23:32:21 -0700

    feat: add temparature and time checks

--- main.py
+++ main.py
@@ -7,7 +7,21 @@ url = f"https://geocoding-api.open-meteo.com/v1/search?name={user_location}"
 response = requests.get(url)
 
 data = response.json()
-print(data['results'][0])
-print(data.keys())
-# print(f'Time: {data["hourly"]["time"][0]}')
-# print(f'Temperature: {data["hourly"]["temperature_2m"][0]}°C')
+longitude = data['results'][0]['longitude']
+latitude = data['results'][0]['latitude']
+
+forecast_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m"
+forecast_response = requests.get(forecast_url)
+
+forecast_data = forecast_response.json()
+time = forecast_data['hourly']['time'][0]
+temperature = forecast_data['hourly']['temperature_2m'][0]
+
+print(f"""
+    Today's weather
+    -----------------
+    Location: {user_location}
+    Time:   {time}
+    Temperature: {temperature}°C
+""")
+ 
